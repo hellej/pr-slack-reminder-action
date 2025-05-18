214910550f591bfe9ed8eef9908dba8fee1c9c34
Joose <joose.helle@iceye.com>
2025-05-18 14:24:24 +0300

    read repo & owner from env

--- cmd/pr-slack-reminder/main.go
+++ cmd/pr-slack-reminder/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 
@@ -11,6 +12,9 @@ func main() {
 	log.Println("Starting PR Slack Reminder...")
 
 	githubToken := os.Getenv("GITHUB_TOKEN")
+	repo := os.Getenv("GITHUB_REPO")
+	repoOwner := os.Getenv("GITHUB_REPO_OWNER")
+
 	// slackWebhook := os.Getenv("SLACK_WEBHOOK_URL")
 
 	if githubToken == "" {
@@ -20,5 +24,15 @@ func main() {
 	client := githubclient.GetClient(githubToken)
 
 	log.Printf("GitHub client created successfully %s", client.BaseURL.String())
+
+	prs, _, err := client.PullRequests.List(context.Background(), repoOwner, repo, nil)
+
+	if err != nil {
+		log.Fatalf("Error fetching pull requests: %v", err)
+	}
+
+	for _, pr := range prs {
+		log.Printf("PR: %s, Title: %s", pr.GetHTMLURL(), pr.GetTitle())
+	}
 
 }
