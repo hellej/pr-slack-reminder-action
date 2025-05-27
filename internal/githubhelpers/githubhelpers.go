9ef512a4039e110f0a1c54e11add47bf28540436
Joose <joose.helle@gmail.com>
2025-05-27 21:54:58 +0300

    sort PRs by created time

--- internal/githubhelpers/githubhelpers.go
+++ internal/githubhelpers/githubhelpers.go
@@ -3,6 +3,7 @@ package githubhelpers
 import (
 	"context"
 	"log"
+	"slices"
 	"strings"
 
 	"github.com/google/go-github/v72/github"
@@ -23,6 +24,25 @@ func parseOwnerAndRepo(repository string) (string, string) {
 	return repoOwner, repoName
 }
 
+func sortPRsByCreatedAt(prs []*github.PullRequest) []*github.PullRequest {
+	slices.SortFunc(prs, func(a, b *github.PullRequest) int {
+		if a.GetCreatedAt().After(b.GetCreatedAt().Time) {
+			return -1
+		}
+		if a.GetCreatedAt().Before(b.GetCreatedAt().Time) {
+			return 1
+		}
+		if a.GetUpdatedAt().After(b.GetUpdatedAt().Time) {
+			return -1
+		}
+		if a.GetUpdatedAt().Before(b.GetUpdatedAt().Time) {
+			return 1
+		}
+		return 0
+	})
+	return prs
+}
+
 func FetchOpenPRs(client *github.Client, repository string) []*github.PullRequest {
 	repoOwner, repoName := parseOwnerAndRepo(repository)
 	log.Printf("Fetching PRs from repository: %s/%s", repoOwner, repoName)
@@ -33,5 +53,6 @@ func FetchOpenPRs(client *github.Client, repository string) []*github.PullReques
 	for _, pr := range prs {
 		log.Printf("PR: %s, Title: %s", pr.GetHTMLURL(), pr.GetTitle())
 	}
-	return prs
+
+	return sortPRsByCreatedAt(prs)
 }
