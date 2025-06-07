78a307634615f1a0678d036e1a09e2c59447bcae
Joose <joose.helle@gmail.com>
2025-06-07 19:31:27 +0300

    add test

--- cmd/pr-slack-reminder/run_test.go
+++ cmd/pr-slack-reminder/run_test.go
@@ -1,7 +1,6 @@
 package main_test
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/google/go-github/v72/github"
@@ -25,33 +24,75 @@ func makeMockGitHubClientGetter(prs []*github.PullRequest) func(token string) gi
 	}
 }
 
-type mockSlackClient struct{}
+type SentMessage struct {
+	channelID   string
+	blocks      slack.Message
+	summaryText string
+}
+
+type mockSlackClient struct {
+	sentMessage SentMessage
+}
 
 func (c mockSlackClient) GetChannelIDByName(channelName string) (string, error) {
 	return "C12345678", nil
 }
 
-func (c mockSlackClient) SendMessage(channelID string, blocks slack.Message, summaryText string) error {
-	return errors.New("mock error: failed to send message")
+func (c *mockSlackClient) SendMessage(channelID string, blocks slack.Message, summaryText string) error {
+	c.sentMessage = SentMessage{
+		channelID:   channelID,
+		blocks:      blocks,
+		summaryText: summaryText,
+	}
+	return nil
 }
 
-func getSlackClient(token string) slackclient.Client {
-	return mockSlackClient{}
+func asSlackClientGetter(client *mockSlackClient) func(token string) slackclient.Client {
+	return func(token string) slackclient.Client {
+		return client
+	}
 }
 
-func TestRun(t *testing.T) {
+func setTestEnvironment(t *testing.T, noPrsMessage string) {
 	t.Setenv("GITHUB_REPOSITORY", "test-org/test-repo")
 	t.Setenv("INPUT_GITHUB-TOKEN", "SOME_TOKEN")
 	t.Setenv("INPUT_SLACK-BOT-TOKEN", "SOME_TOKEN")
 	t.Setenv("INPUT_SLACK-CHANNEL-NAME", "some-channel-name")
 	t.Setenv("INPUT_GITHUB-USER-SLACK-USER-ID-MAPPING", "someuser: USOMEID")
 	t.Setenv("INPUT_MAIN-LIST-HEADING", "There are <pr_count> open PRs 🚀")
+	if noPrsMessage != "" {
+		t.Setenv("INPUT_NO-PRS-MESSAGE", noPrsMessage)
+	}
 
-	t.Run("No PRs found", func(t *testing.T) {
-		getGitHubClient := makeMockGitHubClientGetter([]*github.PullRequest{})
-		err := main.Run(getGitHubClient, getSlackClient)
-		if err != nil {
-			t.Errorf("Expected no error, got: %v", err)
-		}
-	})
+}
+
+func TestNoPRsFoundWithoutMessage(t *testing.T) {
+	setTestEnvironment(t, "")
+	getGitHubClient := makeMockGitHubClientGetter([]*github.PullRequest{})
+	mockSlackClient := mockSlackClient{}
+	err := main.Run(getGitHubClient, asSlackClientGetter(&mockSlackClient))
+	if err != nil {
+		t.Errorf("Expected no error, got: %v", err)
+	}
+	if mockSlackClient.sentMessage.summaryText != "" {
+		t.Errorf("Expected no message to be sent, but got: %v", mockSlackClient.sentMessage.summaryText)
+	}
+}
+
+func TestNoPRsFoundWithMessage(t *testing.T) {
+	noPRsFoundMessage := "No PRs found, happy coding!"
+	setTestEnvironment(t, noPRsFoundMessage)
+	getGitHubClient := makeMockGitHubClientGetter([]*github.PullRequest{})
+	mockSlackClient := mockSlackClient{}
+	err := main.Run(getGitHubClient, asSlackClientGetter(&mockSlackClient))
+	if err != nil {
+		t.Errorf("Expected no error, got: %v", err)
+	}
+	if mockSlackClient.sentMessage.summaryText != noPRsFoundMessage {
+		t.Errorf(
+			"Expected summary to be %v, but got: %v",
+			noPRsFoundMessage,
+			mockSlackClient.sentMessage.summaryText,
+		)
+	}
 }
